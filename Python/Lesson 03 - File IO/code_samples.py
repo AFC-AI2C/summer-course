d7643a3a813f78fff40c03447c9395f8a8a99695
recursivefunctions <cyber.sector642@passmail.com>
2025-12-29 09:31:51 -0500

    added some more comments to code_samples.py

--- Python/Lesson 03 - File IO/code_samples.py
+++ Python/Lesson 03 - File IO/code_samples.py
@@ -15,15 +15,24 @@ with open('output.txt', 'w') as file:
     for text in text_to_write:
         file.write(text)
 
-
+#read the contents of a file and store it in the variable 'content'
 file_handle = open('output.txt', 'r')
 content = file_handle.read()
+#you have to manually close it when using this syntax
 file_handle.close()
 
+#this syntax handles the file opening and closing automatically
+#so you can just focus on assigning the contents to a variable
 with open('output.txt', 'r') as file_handle:
     content = file_handle.read()
 
+# Open 'input.txt' in read mode and 'output.txt' in write mode using context managers
+# This ensures files are properly closed after operations
 with open('input.txt', 'r') as input, open('output.txt', 'w') as output:
+# Iterate through each line in the input file
     for line in input:
+        # Strip whitespace from the beginning and end of the line,
+        # convert it to uppercase, add a newline character,
+        # and write it to the output file
         output.write(line.strip().upper() + '\n')
 

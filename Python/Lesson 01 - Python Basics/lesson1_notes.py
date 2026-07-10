00a48e697418044575a3f444e6c17a5761c704db
Benjamin Greenspan <benjamin.greenspan@gmail.com>
2026-07-10 15:49:23 -0400

    Attempt for summer-course\Python\Lesson 01 - Python Basics for in class stretch exercises 1 and 2

--- Python/Lesson 01 - Python Basics/lesson1_notes.py
+++ Python/Lesson 01 - Python Basics/lesson1_notes.py
@@ -1,3 +1,4 @@
+"""
 name = input("What is your name? ")
 favorite_number = int(input("What is your favorite number? "))
 #print("Hello " + name)
@@ -6,3 +7,24 @@ favorite_number = int(input("What is your favorite number? "))
 print("Hello ", name)
 print("Your favorite number is ", favorite_number)
 print("Your favorite number minus 10 is ", favorite_number - 10)
+"""
+
+diameter_deal = input("What is the diameter for each small pizza? ")
+area_small = 3.14 * (float(diameter_deal)/2) ** 2
+area_deal = 2 * area_small
+print(f"The amount of pizza in the deal as an area is {area_deal:.02f} square units.")
+cost_deal = input("What is the cost of the 2 small pizza deal? ")
+print(f"The cost per square unit of area of pizza in the deal is ${(float(cost_deal)/area_deal):.02f} per square unit.")
+
+diameter_large = input("What is the diameter of the large pizza? ")
+area_large = 3.14 * (float(diameter_large)/2) ** 2
+print(f"The amount of area in the large pizza is {area_large:.02f} square units.")
+cost_large = input("What is the cost of the large pizza? ")
+print(f"The cost per square unit of area of large pizza is ${(float(cost_large)/area_large):.02f} per square unit.")
+
+if (float(cost_large)/area_large) < (float(cost_deal)/area_deal):
+    print("The large pizza is a better deal.")
+elif (float(cost_large)/area_large) > (float(cost_deal)/area_deal):
+    print("The two pizzas are a better deal.")
+else:
+    print("The pizzas are the same price.")

520b834b1af74d4e154e30ba73b1b0a9528c2f93
Benjamin Greenspan <benjamin.greenspan@gmail.com>
2026-07-26 18:57:57 -0400

    Added Lesson 4 notes

--- Python/Lesson 04 - Strings, Advanced Functions, and Virtual Environments/area.py
+++ Python/Lesson 04 - Strings, Advanced Functions, and Virtual Environments/area.py
@@ -2,7 +2,14 @@ def rectangle_area(length: float, width: float) -> float:
     return length * width
 
 def circle_area(radius: float) -> float:
+<<<<<<< HEAD
     return 3.14 * radius ** 2
 
 def tri_area(base: float, height: float) -> float:
     return base * height / 2
+=======
+    return 3.1415 * radius **2
+
+def tri_area(base: float, height: float) -> float:
+    return 0.5 * base * height
+>>>>>>> a15d1c1 (Added Lesson 4 notes)

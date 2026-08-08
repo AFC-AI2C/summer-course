c2de09d45155273a79babd0fe6438b146a49f112
unknown <gina.avila07@gmai.com>
2026-08-08 16:28:16 -0500

    Problem set 5, #2 2nd try

--- Python/Weekly Problem Sets/Problem Set 5.py
+++ Python/Weekly Problem Sets/Problem Set 5.py
@@ -90,7 +90,7 @@ def fibonacci(n:int)-> int:
     if n == 1:
         return 1
 
-    return fibonacci(n - 1) + (n - 2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def count_ways(n:int)->int:
